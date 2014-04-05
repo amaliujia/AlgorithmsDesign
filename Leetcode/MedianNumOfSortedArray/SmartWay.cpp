fec5c8e95c85b7f73cc50abfc9c59844b4c0a80d
amaliujia <amaliujia@163.com>
2014-04-05 21:07:14 +0800

    Far more smarter way for searching median

--- Leetcode/MedianNumOfSortedArray/SmartWay.cpp
+++ Leetcode/MedianNumOfSortedArray/SmartWay.cpp
@@ -5,11 +5,35 @@ double findMedianSortedArrays(int A[], int m, int B[], int n) {
     if(sum % 2)
     {
         return findkth(A, m, B, n, sum/2+1);
+    }else{
+        return (findkth(A, m, B, n, sum/2) + (findkth(A, m, B, n, sum/2+1))) / 2.0;
     }
 }
 
 double findkth(int A[], int m, int B[], int n, int k)
 {
+    int ka,kb;
+    if(m > n)
+    {
+        return findkth(B, n, A, m, k);
+    }
+    if(m == 0){
+        return B[k-1];
+    }
+    if(k == 1){
+        return A[0] < B[0] ? A[0] : B[0]; 
+    }
+    
+    ka = k/2 > m ? m : k/2;
+    kb = k - ka;
+    
+    if(A[ka-1] == B[kb]){
+        return A[ka-1];
+    }else if(A[ka-1] > B[kb-1]){
+        return findkth(A, m, B+kb, n-kb, k-kb);   
+    }else{
+        return findkth(A+ka, m-ka, B, n, k-ka);
+    }
     
 }
 };
