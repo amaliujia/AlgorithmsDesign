6323e0cd46177d9375f7424d2e296af7094bbc93
amaliujia <amaliujia@163.com>
2014-04-08 22:32:57 +0800

    Queue by linked list

--- CC/myDataStructure/QueueLinkedList.java
+++ CC/myDataStructure/QueueLinkedList.java
@@ -1,3 +1,105 @@
 /*
  * Queue implementation with linked list
-/*
+*/
+public class LinkedQueue<T>
+{
+	private class Node
+	{
+		private T data;
+		private Node next;
+		public Node()
+		{
+			next = null;
+			data  = null;
+		}
+		public Node(T data, Node next)
+		{
+			this.data = data;
+			this.next = next;
+		}
+	}
+	
+	private Node front;
+	private Node rear;
+	private int size;
+	
+	public LinkedQueue()
+	{
+		front = null;
+		rear = null;
+		size = 0;
+	}
+		
+	public LinkedQueue(T element)
+	{
+		front  = new Node(element, null);
+		rear = front;
+		size++;
+	} 
+	
+	public int length()
+	{
+		return size;
+	}
+
+	public void insert(T element)
+	{
+		if(front == null)
+		{
+			front = new Node(element, null);
+			rear = front;	
+		}else{
+			new aNode = new Node(element, null);
+			rear.next = aNode;
+			rear = aNode;
+		}
+		size++;
+	}
+		
+	public T remove()
+	{
+		Node old = front;
+		front = front.next;
+		old.next = null;
+		size--;
+		return old.data; 
+	}
+
+	public T element()
+	{
+		return front.data;
+	}
+
+	public boolean empty()
+	{
+		return size == 0;
+	}
+
+	public void clear()
+	{
+		front = null;
+		rear = null;
+		size = 0; 
+	}
+
+	public String toString()
+	{
+		if(empty())
+		{
+			return "[]";
+		}else{
+			StringBuffer buffer = new StringBuffer();
+			Node iterator = front;
+			buffer.append("[");
+			while(iterator.next != null)
+			{
+				buffer.append(iterator.data);
+				buffer.append(", ");
+				iterator = iterator.next;
+			}
+			buffer.append(iterator.data);
+			buffer.append("]"		    	
+			return buffer.toString();
+		}
+	}
+}
