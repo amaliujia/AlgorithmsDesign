10ca133523396460d8bf3e67c4485b942aa00ea0
amaliujia <amaliujia@163.com>
2014-05-01 12:41:34 +0800

    change

--- Leetcode/ReverseInt/ReverseInteger.cpp
+++ Leetcode/ReverseInt/ReverseInteger.cpp
@@ -1,27 +1,27 @@
-    int reverse(int x) {
-       int intermedia = 0;
-       int result = 0;
-       bool isNegative = x < 0 ? true : false;
+int reverse(int x) {
+    int intermedia = 0;
+    int result = 0;
+    bool isNegative = x < 0 ? true : false;
        
-	   if(isNegative){
-           x = -x;
-       }
-       
-       while(x != 0)
-       {
-           intermedia = x - x / 10 * 10;
-           result = result * 10 + intermedia;
-           x = x / 10;
-       }
-		
-	   if(result < 0)
-	   {
-		   return -1; //overflow, so result becomes negative which is impossible arithmetically.
-	   }		
-
-       if(isNegative)
-       {
-          result = -result;
-       }
-       return result;
+	if(isNegative){
+        x = -x;
     }
+       
+    while(x != 0)
+   	{
+       intermedia = x - x / 10 * 10;
+       result = result * 10 + intermedia;
+       x = x / 10;
+    }
+		
+	if(result < 0)
+	{
+		return -1; //overflow, so result becomes negative which is impossible arithmetically.
+	}		
+
+    if(isNegative)
+    {
+       result = -result;
+    }
+    return result;
+}
