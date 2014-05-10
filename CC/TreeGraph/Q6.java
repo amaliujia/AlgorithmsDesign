edbe0574ba7cfd538ead570b6b93e7d11456e119
amaliujia <amaliujia@163.com>
2014-05-10 10:28:30 +0800

    full version of this question

--- CC/TreeGraph/Q6.java
+++ CC/TreeGraph/Q6.java
@@ -1,11 +1,20 @@
 public static TreeNode findNextBST(TreeNode node){
 	if(node.parent == null) return null;
 	
-	if(node.right != null) return node.right;
-
-	if(node.parent.left == node){
-		return node.parent;
-	}else{
-		return findNextBST(node.parent);
+	if(node.right != null) return leftMostChild(node.right);
+	else{
+		TreeNode pre = node;
+		TreeNode x = node.parent;
+		while(x != null && x.left != node){
+			pre = x;
+			x = x.parent;
+		}
+		return x;		
 	}
+}
+
+public static TreeNode leftMostChild(TreeNode n){
+	while(n.left != null)
+		n = n.left;
+	return n;	
 }
