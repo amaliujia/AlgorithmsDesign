56afeb3c23544100c89222e24f33ed3048214fc6
amaliujia <amaliujia@163.com>
2014-05-11 18:02:44 +0800

    level order traversal recursion

---
+++ Leetcode/BSTLevelOrderTraversal/BSTLevelOrderTraversal.cpp
@@ -0,0 +1,20 @@
+/*
+	O(n) time
+	O(n) space
+*/
+
+class Solution {
+public:
+      vector<vector<int> > levelOrder(TreeNode *root) {
+          vector<vector<int>> result;
+          traverse(root, 1, result);
+          return result;
+}
+      void traverse(TreeNode *root, size_t level, vector<vector<int>> &result) {
+          if (!root) return;
+          if (level > result.size())
+              result.push_back(vector<int>());
+          result[level-1].push_back(root->val);
+          traverse(root->left, level+1, result);
+          traverse(root->right, level+1, result);
+} };
