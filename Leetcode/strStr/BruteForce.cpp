b3239a502b1ff3c1bc717a2ce0b59cf0f442101d
amaliujia <amaliujia@163.com>
2014-06-08 20:59:09 +0800

    strStr by brute force in Java

--- Leetcode/strStr/BruteForce.cpp
+++ Leetcode/strStr/BruteForce.cpp
@@ -1,3 +1,9 @@
+/*
+	Say length of haystack is m, the length of needle is n.  
+	Best case O(m + n) time.
+	Worst case O(mn) time.
+	Average case O(mn) time.
+*/
 char *strStr(char *haystack, char *needle) {
 	if(haystack == NULL || needle == NULL)	return NULL;
     if((*haystack == '\0') && (*needle == '\0'))  return "";
