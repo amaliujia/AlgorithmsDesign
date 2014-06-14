9754136c44c3d1836214a37945fed23f47b3655f
amaliujia <amaliujia@163.com>
2014-06-14 13:49:38 +0800

    merge two sorted array

---
+++ Leetcode/MergeSortedArray/spaceEfficient.cpp
@@ -0,0 +1,9 @@
+void merge(int A[], int m, int B[], int n){
+	int pa = m - 1, pb = n -1, pt = m + n - 1;
+	
+	while(pa >= 0 && pb >= 0){
+		A[pt--] = (A[pa] > B[pb]) ?  A[pa--] : B[pb--]; 
+	}
+
+	while(pb >= 0)	A[pt--] = B[pb--];
+}
