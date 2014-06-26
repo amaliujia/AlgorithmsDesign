51fdd0f17abaecc5603488e710a947482ff0ecb2
amaliujia <amaliujia@163.com>
2014-06-26 23:55:44 +0800

    add binary in string

--- Leetcode/AddBinary/solution.cpp
+++ Leetcode/AddBinary/solution.cpp
@@ -1,37 +1,19 @@
-class Solution {
+ss Solution {
 public:
     string addBinary(string a, string b) {
         string result = "";
         reverse(a.begin(), a.end());
         reverse(b.begin(), b.end());
         int carry = 0;
-        int bound = (a.length() < b.length()) ? a.length() : b.length();
+        int bound = (a.length() > b.length()) ? a.length() : b.length();
         for(int i = 0; i < bound; i++){
-            if(a[i] == '1' && b[i] == '1'){
-                if(carry == 1){
-                    result[i] = '1';
-                }else{
-                    result[i] = '0';
-                }
-                carry = 1;
-            }else if(a[i] == '1' || b[i] == '1'){
-                if(carry == 1){
-                    result[i] = '0';
-                    carry = 1;
-                }else{
-                    result[i] = '0';
-                    carry = 0;
-                }                
-            }else{
-                if(carry == 1){
-                    result[i] = '1';
-                }else{
-                    result[i] = '0';
-                }
-                carry = 0;
-            }
+            int ia = i < a.size() ? a[i] - '0' : 0;
+            int ib = i < b.size() ? b[i] - '0' : 0;
+            int c = (ia + ib + carry) % 2;
+            carry = (ia + ib + carry) / 2;
+            result.insert(result.begin(), c + '0');
         }
-    
+        if(carry == 1)  result.insert(result.begin(), '1');
         return result;
     }
 };
