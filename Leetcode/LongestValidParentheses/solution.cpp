0476c27bae08aa30b44a4f412b7a5fe43ee51616
amaliujia <amaliujia@163.com>
2014-07-22 16:34:24 -0400

    Longest Valid Parentheses

---
+++ Leetcode/LongestValidParentheses/solution.cpp
@@ -0,0 +1,23 @@
+// O(n) time
+// O(n) space
+	int longestValidParentheses(string s) {
+        int result = 0, last = -1;
+        stack<int> c;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '('){
+                c.push(i);
+            }else{
+                if(c.empty()){
+                    last = i;
+                }else{
+                    c.pop();
+                    if(c.empty()){
+                        result = max(result, i - last); 
+                    }else{
+                        result = max(result, i - c.top());
+                    }
+                }
+            }
+        }
+        return result;
+    }
