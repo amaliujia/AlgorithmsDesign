8a355b7a154b36449b50cd798ef2c537bf3a586a
amaliujia <amaliujia@163.com>
2014-08-05 23:57:55 +0800

    regular expression resursion

---
+++ Leetcode/WildcardMatching/resursion.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+      return innerMatch(s, p);  
+    }
+    
+	bool innerMatch(const char *a, const char *b){
+		 if((*a == '\0' && *b != '\0') || (*a != '\0' && *b == '\0')){
+		     return false;
+		 }
+    
+		if(*a == '\0' && *b == '\0'){
+			 return true;
+		 }
+    
+		if(*b == '?'){
+			 return innerMatch(a + 1, b + 1);
+		}
+    
+		if(*b != '*'){
+			 if(*a != *b){
+			     return false;
+			}else{
+				 return innerMatch(a + 1, b + 1);
+			}
+		}
+    
+		int offset = 0;
+    
+		while(1){
+		 if(innerMatch(a + offset, b + 1)){
+		     return true;
+		 }else{
+		     offset++;
+		     if(*(a + offset) == '\0' && *(b + 1) != '\0'){
+	             return false;
+		     }
+        }
+    }
+	 return false;
+ }
+};
