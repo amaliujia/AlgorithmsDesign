a5dd08c103c4d02b3e46affa6ec06e2d0481b772
amaliujia <amaliujia@163.com>
2014-08-11 14:11:22 -0400

    Balanced Binary Tree

---
+++ Leetcode/BalancedBinaryTree/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        if(subTreeDepth(root) == -1){
+            return false;
+        }
+        return true;
+    }
+private:
+    
+    int subTreeDepth(TreeNode *node){
+        if(node == NULL)    return 0;
+        int left = subTreeDepth(node->left);
+        if(left == -1)  return -1;
+        int right = subTreeDepth(node->right);
+        if(right == -1) return -1;
+        
+        if(abs(left - right) > 1){
+            return -1;
+        }
+        return max(left, right) + 1;
+    }
+};
