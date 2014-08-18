270c4ace901bf2b681e7ca505df80515c7fa99b2
amaliujia <amaliujia@163.com>
2014-08-18 19:14:20 -0400

    change

--- Leetcode/JumpGameII/solution.cpp
+++ Leetcode/JumpGameII/solution.cpp
@@ -1,19 +1,19 @@
 // O(n!) time in worse case
 // O(n!) is slower than O(n^2) 
-    int jump(int A[], int n) {
-            if(n == 0)  return 0;
-            if(A[0] > n)   return 1;
-            
-            int closest = n - 1;
-            int min = 0;
-            for(int i = n - 1; i > 0; i--){
-                for(int j = i - 1; j >= 0; j--){
-                    if(A[j] + j >= i){
-                        closest = j;
-                    }
-                }
-                i = closest + 1;
-                min++;
+int jump(int A[], int n) {
+    if(n == 0)  return 0;
+    if(A[0] > n)   return 1;
+          
+    int closest = n - 1;
+    int min = 0;
+    for(int i = n - 1; i > 0; i--){
+        for(int j = i - 1; j >= 0; j--){
+            if(A[j] + j >= i){
+                closest = j;
             }
-            return min;
         }
+        i = closest + 1;
+        min++;
+     }
+     return min;
+}
