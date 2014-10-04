2088d1ffcf5d15f8ab9cce10949f5155de318536
amaliujia <amaliujia@163.com>
2014-10-04 02:23:17 -0400

    add two numbers

---
+++ CodingBootCamp/AddTwoNumbersInLinkedList/solution.cpp
@@ -0,0 +1,23 @@
+Node *addNumbers(Node *n1, Node *n2){
+	if(n1 == NULL)	return n2;
+	if(n2 == NULL)	return n1;
+
+	Node *head = NULL;
+	Node *temp, prev;
+	int carry = 0, sum;	
+	while(n1 != NULL || n2 != NULL){
+		sum = (n1 ? n1->data : 0) + (n2 ? n2->data : 0) + carry;
+		carry = sum / 10;
+		temp = newnode(sum % 10);
+		
+		if(head == NULL)	head = temp;
+		else prev->next = temp;
+		
+		prev = temp;
+
+		if(n1) n1 = n1->next;
+		if(n2) n2 = n2->next;	
+	}
+	if(carry)	prev->next = newnode(carry);				
+	return head;
+}	
