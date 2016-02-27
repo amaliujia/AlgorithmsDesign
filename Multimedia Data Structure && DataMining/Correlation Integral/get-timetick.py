1b18bbe7dde82bc4f4557739f28f58e424c7000c
amaliujia <amaliujia@163.com>
2016-02-27 14:49:05 -0500

    update

--- Multimedia Data Structure && DataMining/Correlation Integral/get-timetick.py
+++ Multimedia Data Structure && DataMining/Correlation Integral/get-timetick.py
@@ -11,17 +11,35 @@
 ###########################################################
 
 import sys
+import math
 import matplotlib.pyplot as plt
 
 def L1(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]) + abs(p1[2] - p2[2])
 
+def L2(p1, p2):
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+
+def D2DimensionNavieL2(data=[]):
+    ret_x = []
+    ret_y = []
+    r = 0.0
+    for i in range(0, 400):
+        ret_x.append(r)
+        count = 0
+        for p in data:
+            for q in data:
+                if (L2(p, q) <= r):
+                    count += 1
+        ret_y.append(count)
+        r += 0.01
+    return ret_x, ret_y
+
 def D2DimensionNavieL1(data=[]):
     ret_x = []
     ret_y = []
     r = 0.0
-    for i in range(0, 100):
-        r += 0.01
+    for i in range(0, 400):
         ret_x.append(r)
         count = 0
         for p in data:
@@ -29,6 +47,7 @@ def D2DimensionNavieL1(data=[]):
                 if (L1(p, q) <= r):
                     count += 1
         ret_y.append(count)
+        r += 0.01
     return ret_x, ret_y
 
 
@@ -39,6 +58,7 @@ if __name__ == "__main__":
 
     filename = sys.argv[1]
     timetick = int( sys.argv[2])
+    LD = int(sys.argv[3])
 
     infile = open(filename, 'r')
 
@@ -53,8 +73,14 @@ if __name__ == "__main__":
             data.append([float(columns[0]), float(columns[1]), float(columns[2])])
 
     infile.close()
+    x_axis = []
+    y_axis = []
+    if (LD == 0):
+        x_axis, y_axis = D2DimensionNavieL1(data)
 
-    x_axis, y_axis = D2DimensionNavieL1(data)
+    if (LD == 1):
+        x_axis, y_axis = D2DimensionNavieL2(data)
+        
     plt.plot(x_axis, y_axis)
     print x_axis
     print y_axis
