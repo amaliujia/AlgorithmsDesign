712b893f69daa66bb0b377b089d559802747726a
amaliujia <amaliujia@163.com>
2016-02-27 15:30:05 -0500

    power laws

---
+++ Multimedia Data Structure && DataMining/Power Law/q2.py
@@ -0,0 +1,14 @@
+#######################
+# Author: yourName
+# Date: today
+# USAGE: 
+#         python q2.py
+# to generate all plots for HW3-Q2
+######################
+import math
+
+print "HW3-Q2: implement me  - dont worry about output format"
+def gaussian_distribution(mu=0, sigma=0.01, x=[]):
+  y=[]; 
+  for i in x:
+    r = 1.0 / (math.sqrt())
